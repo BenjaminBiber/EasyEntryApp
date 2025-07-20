1ca169a3d9e122e009f0c1bd8f34053e6ef19e32
Benjamin Biber (Linux) <ben.biber.strass@gmail.com>
2025-07-20 16:18:55 +0200

    Update API URL in HttpClient configuration

--- EasyEntryApp/Program.cs
+++ EasyEntryApp/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Environment.GetEnvironmentVariable("API_URL") ?? "http://192.168.50.225:5190") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Environment.GetEnvironmentVariable("API_URL") ?? "https://easyentryapi.benjaminbiber.de") });
 builder.Services.AddScoped<SettingService>();
 builder.Services.AddScoped<DeviceGroupService>();
 builder.Services.AddScoped<DeviceService>();
